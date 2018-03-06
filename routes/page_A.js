3ee28b9e5e4f9cf7342405539a6b88e89a3bb485
Lynley Yamaguchi <lynleyyama@yahoo.com>
2018-03-06 14:10:27 -0800

    search bar analytics

--- routes/page_A.js
+++ routes/page_A.js
@@ -6,3 +6,11 @@
 exports.view = function(req, res){
   res.render('page_A');
 };
+
+function initializePage() {
+	// your code here
+	$("#searchbtn").click(function(){
+		ga('create','UA-114623091-2','auto');
+		ga('send', 'event', 'Search', 'click');
+	});
+}
