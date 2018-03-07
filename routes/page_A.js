bd4fb9ddeff70ed4b8ca2a07174ded8f6ed4778e
Lynley Yamaguchi <lynleyyama@yahoo.com>
2018-03-06 16:44:19 -0800

    ga categories

--- routes/page_A.js
+++ routes/page_A.js
@@ -16,5 +16,41 @@ function initializePage() {
         ga('send', 'event', 'Search', 'click');
         ga('send', 'pageview');
       });
+      $("#category1").click(function(){
+        e.preventDefault();
+        ga('create','UA-114623091-2','auto');
+        ga('send', 'event', 'category1', 'click');
+        ga('send', 'pageview');
+      });
+      $("#category2").click(function(){
+        e.preventDefault();
+        ga('create','UA-114623091-2','auto');
+        ga('send', 'event', 'category2', 'click');
+        ga('send', 'pageview');
+      });
+      $("#category3").click(function(){
+        e.preventDefault();
+        ga('create','UA-114623091-2','auto');
+        ga('send', 'event', 'category3', 'click');
+        ga('send', 'pageview');
+      });
+      $("#category4").click(function(){
+        e.preventDefault();
+        ga('create','UA-114623091-2','auto');
+        ga('send', 'event', 'category4', 'click');
+        ga('send', 'pageview');
+      });
+      $("#category5").click(function(){
+        e.preventDefault();
+        ga('create','UA-114623091-2','auto');
+        ga('send', 'event', 'category5', 'click');
+        ga('send', 'pageview');
+      });
+      $("#category6").click(function(){
+        e.preventDefault();
+        ga('create','UA-114623091-2','auto');
+        ga('send', 'event', 'category6', 'click');
+        ga('send', 'pageview');
+      });
     });
 }
