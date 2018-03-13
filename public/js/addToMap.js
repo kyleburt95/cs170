f54047374c032cfc717690254c25f566525f75b7
Kyle Burt <k1burt@ucsd.edu>
2018-03-12 22:58:41 -0700

    before pull

--- public/js/addToMap.js
+++ public/js/addToMap.js
@@ -11,7 +11,7 @@ function onMapClick(e) {
 
 
 // Using leaflet.js to pan and zoom a big image.
-    var map = L.map('image-map', {
+    var map = L.map('add-map', {
       minZoom: 1,
       maxZoom: 4,
       center: [0, 0],
@@ -44,16 +44,16 @@ function onMapClick(e) {
 
 //to get dropdown menu to toggle
 $(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
-    placedPopup = L.popup()
-        .setLatLng([map.getCenter().lat, map.getCenter().lng ])
-        .setContent("Type in field below to change text, and tap to change location")
-        .openOn(map);
-  
-    $('#saveButton').click(update);
+  $(".dropdown-toggle").dropdown();
+  placedPopup = L.popup()
+      .setLatLng([map.getCenter().lat, map.getCenter().lng ])
+      .setContent("Type in field below to change text, and tap to change location")
+      .openOn(map);
+
+  $('#saveButton').click(update);
 });
   
-$("#markerTextInput").change(textChanged);
+$("#popups").change(textChanged);
   
 function textChanged() {
   placedPopup.setContent($(this).val())
